9a283fa4263b0754461b35d97298431f0ab76bfc
Delmac <delalitengue.dev@gmail.com>
2023-09-20 14:10:12 +0000

    added three loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Default from "./designs/Default";
 import Discord_Card_1 from "./designs/Discord_Card_1";
 import InstagramCard_1 from "./designs/InstagramCard_1";
+import Circle_Loader_1 from "./designs/Circle_Loader_1";
+import Circle_Loader_2 from "./designs/Circle_Loader_2";
+import Circle_Loader_3 from "./designs/Circle_Loader_3";
 
 function App() {
 
   return (
-    <main className="w-full min-h-screen bg-cyan-100 grid grid-cols-1 sm:grid-cols-2">
+    <main className="w-full min-h-screen grid grid-cols-1 sm:grid-cols-2">
       <Default />
       <Discord_Card_1 />
       <InstagramCard_1 />
+      <Circle_Loader_1 />
+      <Circle_Loader_2 />
+      <Circle_Loader_3 />
     </main>
   )
 }
