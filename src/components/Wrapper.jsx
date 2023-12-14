e0ae46cdab6f6168fbf9ff8620877d09003daf9c
delmac <delalitengue.dev@gmail.com>
2023-12-14 01:23:51 -0800

    made edits

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Wrapper = ({children,bgcolor, title, txtcolor}) => {
   return (
-    <div className={`${bgcolor ?? 'bg-background'} flex flex-col justify-between items-center min-h-[300px]`}>
+    <div className={`${bgcolor ?? 'bg-background'} relative flex flex-col justify-between items-center min-h-[300px]`}>
       <div className='flex-1 w-full h-full flex items-center justify-center p-6'>
         {children}
       </div>
---
+++ src/designs/Star_Rating.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../components/Wrapper'
+
+const Star_Rating = () => {
+  return (
+    <Wrapper title={"Star Trick"} >
+        something
+    </Wrapper>
+  )
+}
+
+export default Star_Rating
