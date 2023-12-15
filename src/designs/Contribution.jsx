763f984b88b26702e7fab96e390cd75ea53d769e
delmac <delalitengue.dev@gmail.com>
2023-12-15 01:04:03 -0800

    made edits

--- src/designs/Contribution.jsx
+++ src/designs/Contribution.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import Wrapper from '../components/Wrapper';
+import { AiFillDelete } from 'react-icons/ai';
+import { Input } from "@/components/ui/input"
 
 const data = [
     //type: TEXT
@@ -71,7 +73,22 @@ export default Contribution;
 const Card = () => {
     return (
         <div className="relative w-[640px] min-h-[240px] rounded-lg bg-card border border-border overflow-hidden">
-            <div className="w-full h-11 bg-secondary/50 border-b border-border"></div>
+            <div className="w-full h-11 bg-secondary/30 border-b border-border flex items-center justify-between px-4">
+                <h2 className="py-1 px-2.5 border rounded-full text-secondary-foreground/80 text-sm">Anonymous</h2>
+                <button className="rounded-md p-1.5 hover:bg-destructive/20 text-destructive">
+                    <AiFillDelete size={18} />
+                </button>
+            </div>
+            <div className="w-full min-h-full p-5">
+                <div className="w-full mb-4">
+                    <label htmlFor="school" className='block mb-3 text-card-foreground/50'>School Name</label>
+                    <Input id="school" type="text" className="w-3/4" />
+                </div>
+                <div className="w-full mb-4">
+                    <label htmlFor="motto" className='block mb-3 text-card-foreground/50'>Motto</label>
+                    <Input id="motto" type="text" className="w-3/4" />
+                </div>
+            </div>
         </div>
     )
 }
