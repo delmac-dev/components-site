b9ec7a72c2d118a577788b062c47336b4e94b40e
delmac <delalitengue.dev@gmail.com>
2023-12-26 12:43:56 -0800

    made edits

--- src/designs/RadioPoint.jsx
+++ src/designs/RadioPoint.jsx
@@ -3,14 +3,25 @@ import './styles/point.css';
 
 const RadioPoint = () => {
   return (
-    <Wrapper title={"Radio Point"}>
-        <div className="flex gap-12 flex-col">
-            {/* Point one */}
-            <div className="shd2 aspect-square rounded-full w-8 bg-secondary cursor-pointer relative"></div>
+    <Wrapper title={"Radio Point"} bgcolor={""}>
+        <div className="flex gap-12">
+            <Point />
+            <Point active />
 
         </div>
     </Wrapper>
   )
 }
 
-export default RadioPoint
+export default RadioPoint;
+
+
+const Point = ({active}) => {
+  return (
+    <div className="shd rounded-full w-11 bg-secondary aspect-square flex items-center justify-center">
+      <div className="shd2 rounded-full w-8 aspect-square bg-secondary/80 cursor-pointer flex items-center justify-center">
+        {active ? <div className="bg-primary/80 shd3 aspect-square w-5 rounded-full" /> : ''}
+      </div>
+    </div>
+  )
+}
